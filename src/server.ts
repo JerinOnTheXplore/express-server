505586abc6ef88a00df022bd9080a870cdec391b
JerinXplore <jerinjerin101325@gmail.com>
2025-12-01 11:54:30 +0600

    feat: implemented user update and delete APIs

--- src/server.ts
+++ src/server.ts
@@ -109,6 +109,7 @@ app.post("/users", async(req:Request, res:Response)=>{
   // })
 });
 
+//users CRUD
 app.get("/users",async(req: Request, res: Response)=>{
   try{
     const result = await pool.query(`SELECT * FROM users`);
@@ -142,6 +143,57 @@ app.get("/users/:id", async (req:Request, res:Response)=>{
       success:true,
       message:"User fetched successfully",
       data: result.rows[0],
+    })
+  }
+  } catch(err:any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    })
+  }
+})
+
+//update
+app.put("/users/:id", async (req:Request, res:Response)=>{
+  // console.log(req.params.id);
+  const {name,email} = req.body;
+  try{
+   const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,[name,email,req.params.id]);
+  if(result.rows.length === 0){
+    res.status(404).json({
+      success:false,
+      message:"User not found",
+    });
+  }else{
+    res.status(200).json({
+      success:true,
+      message:"User updated successfully",
+      data: result.rows[0],
+    })
+  }
+  } catch(err:any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    })
+  }
+});
+
+app.delete("/users/:id", async (req:Request, res:Response)=>{
+  // console.log(req.params.id);
+  try{
+   const result = await pool.query(`DELETE FROM users WHERE id = $1`,[req.params.id]);
+  //  console.log(result);
+  if(result.rowCount === 0){
+    res.status(404).json({
+      success:false,
+      message:"User not found",
+    });
+  }else{
+    res.status(200).json({
+      success:true,
+      message:"User deleted successfully",
+      data: result.rows,
     })
   }
   } catch(err:any) {
