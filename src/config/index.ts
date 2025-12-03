94f2d2507433a2098b6ac2737698f37ab64cd37a
JerinXplore <jerinjerin101325@gmail.com>
2025-12-03 11:12:31 +0600

    implemented logic for auth module and generated jwt

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,9 @@ dotenv.config({path: path.join(process.cwd(), '.env')});//cwd=current working di
  
 const config = {
     DATABASE_URL: process.env.DATABASE_URL,
-    port: process.env.PORT
+    port: process.env.PORT,
+    secret: process.env.APP_SECRET
+    
 };
 
 export default config;
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,7 @@
-import { pool } from "../../config/db"
-import bcrypt from "bcryptjs"
+import { pool } from "../../config/db";
+import bcrypt from "bcryptjs";
+import jwt from'jsonwebtoken';
+import config from "../../config";
 const loginUser = async (email: string, password: string)=>{
     const result = await pool.query(`SELECT * FROM users WHERE email=$1`,[email]);
     if(result.rows.length === 0) {
@@ -11,4 +13,18 @@ const loginUser = async (email: string, password: string)=>{
     if(!match){
         return false;
     }
+
+//token = payload+secret+ algorithm/koto time pore expire hobe
+
+
+const token = jwt.sign({name: user.name, email: user.email}, config.secret as string, {
+    expiresIn: "7d",
+});
+console.log({token});
+ 
+return {token, user};
+};
+
+export const authServices = {
+    loginUser,
 }
